814722c59a0dcfd6547adb76d07d6b118fb48e37
Manuel Alonso <manuelalonso@invisionapp.com>
2018-03-05 11:06:07 +0100

    fixing sync for getting first instead of create

--- test/unit/lib/elroy-sync.spec.js
+++ test/unit/lib/elroy-sync.spec.js
@@ -6,6 +6,7 @@ const mockery = require("mockery");
 const request = require("request-promise");
 const errors = require("request-promise/errors");
 const EventEmitter = require("events").EventEmitter;
+const nock = require("nock");
 const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised");
 // const expect = chai.expect;
@@ -15,6 +16,7 @@ chai.should();
 const YamlHandler = require("../../../src/util/yaml-handler");
 const EventHandler = require("../../../src/util/event-handler");
 const eventHandler = new EventHandler();
+
 /* used to debug events */
 eventHandler.on("debug", function(msg) {
   console.log(`Received DEBUG message: ${msg}`);
@@ -31,6 +33,8 @@ eventHandler.on("fatal", function(msg) {
 /* */
 
 describe("ElroySync", () => {
+  const ElroySync = require("../../../src/lib/elroy-sync");
+
   describe("match clusters", () => {
     before(function(done) {
       mockery.enable({
@@ -60,7 +64,6 @@ describe("ElroySync", () => {
     });
 
     it("should remove non-existent environments", () => {
-      const ElroySync = require("../../../src/lib/elroy-sync");
       return YamlHandler.loadClusterDefinitions(
         "./test/fixture/clusters"
       ).should.be.fulfilled.then(cldefs => {
@@ -74,7 +77,6 @@ describe("ElroySync", () => {
     });
 
     it("should sync environments", () => {
-      const ElroySync = require("../../../src/lib/elroy-sync");
       return YamlHandler.loadClusterDefinitions(
         "./test/fixture/clusters"
       ).should.be.fulfilled.then(cldefs => {
@@ -84,59 +86,169 @@ describe("ElroySync", () => {
           { elroyUrl: "http://some-url.com", elroySecret: "xxxxx" },
           0
         ).should.be.fulfilled.then(res => {
-          console.log(res);
+          return Promise.resolve(res[0].count).should.eventually.equal(3);
         });
       });
     });
   });
 
-  // describe("Retry Sync", () => {
-  //   let retryCount = 0;
-  //   eventHandler.on("warn", msg => {
-  //     if (msg.includes("retrying")) {
-  //       retryCount++;
-  //     }
-  //   });
-  //   before(function(done) {
-  //     mockery.enable({
-  //       warnOnReplace: false,
-  //       warnOnUnregistered: false,
-  //       useCleanCache: true
-  //     });
-  //     mockery.registerMock("request-promise", function() {
-  //       return Promise.reject(
-  //         new errors.StatusCodeError(504, {}, {}, { statusCode: 504 })
-  //       );
-  //     });
-  //     console.log("Registering mock");
-  //     done();
-  //   });
+  describe("Get Cluster", () => {
+    it("should retrieve cluster", () => {
+      nock("http://elroy_svc")
+        .get("/api/v1/deployment-environment/test")
+        .reply(200, { name: "test" });
 
-  //   after(function(done) {
-  //     mockery.disable();
-  //     mockery.deregisterAll();
-  //     done();
-  //   });
+      return ElroySync.retrieveCluster({ name: "test" }, eventHandler, {
+        elroyUrl: "http://elroy_svc",
+        elroySecret: "xxxxx"
+      }).should.be.fulfilled.then(res => {
+        return Promise.resolve(res.name).should.eventually.equal("test");
+      });
+    });
 
-  //   it("should retry 3 times", done => {
-  //     return Promise.coroutine(function*() {
-  //       const ElroySync = require("../../../src/lib/elroy-sync");
-  //       const clusterDefs = yield YamlHandler.loadClusterDefinitions(
-  //         "./test/fixture/clusters"
-  //       );
-  //       let updated = yield ElroySync.SaveToElroy(
-  //         clusterDefs[0],
-  //         eventHandler,
-  //         { elroyUrl: "http://some-url.com", elroySecret: "xxxxx" },
-  //         retryCount
-  //       );
-  //       done("Should not have reached here");
-  //     })().catch(err => {
-  //       setTimeout(function() {
-  //         expect(retryCount).to.equal(3);
-  //         done();
-  //       }, 3000);
-  //     });
-  //   }).timeout(5000);
-  // });
+    it("should reject when not found cluster", () => {
+      nock("http://elroy_svc")
+        .get("/api/v1/deployment-environment/test")
+        .reply(404, "not found");
+
+      return ElroySync.retrieveCluster({ name: "test" }, eventHandler, {
+        elroyUrl: "http://elroy_svc",
+        elroySecret: "xxxxx"
+      }).should.be.rejectedWith(404);
+    });
+  });
+
+  describe("Sync create/update Cluster", () => {
+    it("should update cluster", () => {
+      nock("http://elroy_svc")
+        .get("/api/v1/deployment-environment/test")
+        .reply(200, { name: "test", metadata: { active: true } });
+
+      nock("http://elroy_svc")
+        .put("/api/v1/deployment-environment/test")
+        .reply(204);
+
+      return ElroySync.SaveToElroy(
+        {
+          cluster: { metadata: { name: "test", actve: true } },
+          resources: () => {}
+        },
+        eventHandler,
+        {
+          elroyUrl: "http://elroy_svc",
+          elroySecret: "xxxxx"
+        },
+        0
+      ).should.be.fulfilled;
+    });
+
+    it("should create cluster", () => {
+      nock("http://elroy_svc")
+        .get("/api/v1/deployment-environment/test")
+        .reply(404, { name: "test", metadata: { active: true } });
+
+      nock("http://elroy_svc")
+        .post("/api/v1/deployment-environment")
+        .reply(204);
+
+      return ElroySync.SaveToElroy(
+        {
+          cluster: { metadata: { name: "test", actve: true } },
+          resources: () => {}
+        },
+        eventHandler,
+        {
+          elroyUrl: "http://elroy_svc",
+          elroySecret: "xxxxx"
+        },
+        0
+      ).should.be.fulfilled;
+    });
+
+    it("should err cluster on creating tier ", () => {
+      nock("http://elroy_svc")
+        .get("/api/v1/deployment-environment/test")
+        .reply(404, "not found cluster");
+
+      nock("http://elroy_svc")
+        .post("/api/v1/deployment-environment")
+        .reply(404, "not found tier");
+
+      return ElroySync.SaveToElroy(
+        {
+          cluster: { metadata: { name: "test", actve: true } },
+          resources: () => {}
+        },
+        eventHandler,
+        {
+          elroyUrl: "http://elroy_svc",
+          elroySecret: "xxxxx"
+        },
+        0
+      ).should.be.rejectedWith("not found tier");
+    });
+
+    it("should err cluster on updating tier ", () => {
+      nock("http://elroy_svc")
+        .get("/api/v1/deployment-environment/test")
+        .reply(200, { name: "test", metadata: { active: true } });
+
+      nock("http://elroy_svc")
+        .put("/api/v1/deployment-environment/test")
+        .reply(404, {
+          message: "not found tier",
+          response: { statusCode: 404 }
+        });
+
+      return ElroySync.SaveToElroy(
+        {
+          cluster: { metadata: { name: "test", actve: true } },
+          resources: () => {}
+        },
+        eventHandler,
+        {
+          elroyUrl: "http://elroy_svc",
+          elroySecret: "xxxxx"
+        },
+        0
+      ).should.be.rejectedWith("not found tier");
+    });
+
+    it("should retry 4 times when error >= 502 ", () => {
+      let retries = 0;
+      let elroyMock = ElroySync;
+      elroyMock.retrieveCluster = (cluster, events, options) => {
+        return Promise.resolve({
+          response: {
+            statusCode: 200,
+            request: { method: "GET" }
+          }
+        });
+      };
+      elroyMock.updateCluster = (cluster, events, options) => {
+        retries++;
+        return Promise.reject({
+          response: {
+            statusCode: 502,
+            request: { method: "PUT" }
+          }
+        });
+      };
+
+      return ElroySync.SaveToElroy(
+        {
+          cluster: { metadata: { name: "test", actve: true } },
+          resources: () => {}
+        },
+        eventHandler,
+        {
+          elroyUrl: "http://elroy_svc",
+          elroySecret: "xxxxx"
+        },
+        0
+      ).should.be.rejected.then(() => {
+        return Promise.resolve(retries).should.eventually.equal(4);
+      });
+    });
+  });
 });
