73663f9a1def78a83d48dc22a11b42982133eea1
Manuel Alonso <manuelalonso@invisionapp.com>
2018-03-21 15:05:01 +0100

    Revert "Revert "setting image for container in generator""

--- test/unit/lib/generator.spec.js
+++ test/unit/lib/generator.spec.js
@@ -1,13 +1,17 @@
 "use strict";
 
 const os = require("os");
-const expect = require("chai").expect;
 const Promise = require("bluebird");
-const yamlHandler = require("../../../src/util/yaml-handler");
+const YamlHandler = require("../../../src/util/yaml-handler");
 const EventHandler = require("../../../src/util/event-handler");
 const Generator = require("../../../src/lib/generator");
 const fse = require("fs-extra");
 const path = require("path");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+const expect = chai.expect;
 
 const configStub = {
   fetch: function() {
@@ -23,67 +27,58 @@ const configStub = {
 };
 
 describe("Generator", () => {
-  describe("with empty cluster", () => {
-    it("should not fail", done => {
-      return Promise.coroutine(function*() {
-        const eventHandler = new EventHandler();
-        const clusterDefs = yield yamlHandler.loadClusterDefinitions(
-          "./test/fixture/empty-clusters"
-        );
-        const clusterDef = clusterDefs[0];
-        const generator = new Generator(
-          clusterDef,
-          {},
-          "",
-          os.tmpdir(),
-          true,
-          configStub,
-          "testrosie",
-          eventHandler
-        );
-        expect(clusterDef).to.exist;
-        yield generator.process();
-        done();
-      })().catch(err => {
-        done(err);
-      });
+  it("should not fail with empty cluster", () => {
+    return YamlHandler.loadClusterDefinitions(
+      "./test/fixture/empty-clusters"
+    ).should.be.fulfilled.then(clusterDefs => {
+      const clusterDef = clusterDefs[0];
+      const generator = new Generator(
+        clusterDef,
+        {},
+        "",
+        os.tmpdir(),
+        true,
+        configStub,
+        "testrosie",
+        new EventHandler()
+      );
+      return generator.process().should.be.fullfilled;
     });
   });
 
-  describe("with valid cluster", () => {
-    it("should create valid service file", done => {
-      return Promise.coroutine(function*() {
-        const clusterDefs = yield yamlHandler.loadClusterDefinitions(
-          "./test/fixture/clusters"
-        );
-        const clusterDef = clusterDefs[1];
-        const generator = new Generator(
-          clusterDef,
-          {},
-          "./test/fixture/resources",
-          os.tmpdir(),
-          true,
-          configStub
-        );
-        expect(clusterDef).to.exist;
-        if (!fse.existsSync(path.join(os.tmpdir(), clusterDef.name()))) {
-          fse.mkdirsSync(path.join(os.tmpdir(), clusterDef.name()));
-        }
-        // manually merge this here
-        clusterDef.configuration().svc = clusterDef.resources().auth.svc;
-        yield generator.processService(
-          clusterDef.resources().auth,
-          clusterDef.configuration()
-        );
-        const svc = yield yamlHandler.loadFile(
-          path.join(os.tmpdir(), clusterDef.name(), "auth-svc.yaml")
-        );
-        expect(svc.metadata.name).to.equal("auth-svc");
-        expect(svc.metadata.labels.app).to.equal("invisionapp");
-        done();
-      })().catch(err => {
-        done(err);
-      });
+  it("should create valid service file with valid cluster", () => {
+    return YamlHandler.loadClusterDefinitions(
+      "./test/fixture/clusters"
+    ).should.be.fulfilled.then(clusterDefs => {
+      const clusterDef = clusterDefs[1];
+      const generator = new Generator(
+        clusterDef,
+        {},
+        "./test/fixture/resources",
+        os.tmpdir(),
+        true,
+        configStub
+      );
+      if (!fse.existsSync(path.join(os.tmpdir(), clusterDef.name()))) {
+        fse.mkdirsSync(path.join(os.tmpdir(), clusterDef.name()));
+      }
+      // manually merge this here
+      clusterDef.configuration().svc = clusterDef.resources().auth.svc;
+
+      generator
+        .processService(clusterDef.resources().auth, clusterDef.configuration())
+        .should.be.fulfilled.then(() => {
+          YamlHandler.loadFile(
+            path.join(os.tmpdir(), clusterDef.name(), "auth-svc.yaml")
+          ).should.be.fulfilled.then(svc => {
+            const expected = {
+              apiVersion: "v1",
+              kind: "Service",
+              metadata: { name: "auth-svc", labels: { app: "invisionapp" } }
+            };
+            Promise.resolve(svc).should.eventually.deep.equal(expected);
+          });
+        });
     });
   });
 
@@ -98,12 +93,20 @@ describe("Generator", () => {
       }
     };
 
-    it("should create copy of config, merging in values from resource", done => {
-      const eventHandler = new EventHandler();
-      return Promise.coroutine(function*() {
-        const clusterDefs = yield yamlHandler.loadClusterDefinitions(
-          "./test/fixture/clusters"
-        );
+    it("should send event to DD without SHA", () => {
+      let events = new EventHandler();
+      events.on("metric", function(msg) {
+        expect(msg).to.deep.equal({
+          kind: "event",
+          title: "No SHA passed in",
+          text: "No SHA passsed in for resource auth",
+          tags: { app: "kit_deploymentizer", kit_resource: "auth" }
+        });
+      });
+
+      return YamlHandler.loadClusterDefinitions(
+        "./test/fixture/clusters"
+      ).should.be.fulfilled.then(clusterDefs => {
         const clusterDef = clusterDefs[3];
         const generator = new Generator(
           clusterDef,
@@ -113,49 +116,75 @@ describe("Generator", () => {
           true,
           configStub,
           undefined,
-          eventHandler
+          events
         );
-        expect(clusterDef).to.exist;
+        // we add the image tag here, since we dont preload the base cluster def in this test
+        clusterDef.resources().auth.containers["auth-con"].image_tag =
+          "node-auth";
+
+        return generator._createLocalConfiguration(
+          clusterDef.configuration(),
+          "auth",
+          clusterDef.resources().auth
+        );
+      });
+    });
+
+    it("should create copy of config, merging in values from resource", () => {
+      return YamlHandler.loadClusterDefinitions(
+        "./test/fixture/clusters"
+      ).should.be.fulfilled.then(clusterDefs => {
+        const clusterDef = clusterDefs[3];
+        const generator = new Generator(
+          clusterDef,
+          imageResources,
+          "./test/fixture/resources",
+          os.tmpdir(),
+          true,
+          configStub,
+          undefined,
+          new EventHandler()
+        );
+
         if (!fse.existsSync(path.join(os.tmpdir(), clusterDef.name()))) {
           fse.mkdirsSync(path.join(os.tmpdir(), clusterDef.name()));
         }
         // we add the image tag here, since we dont preload the base cluster def in this test
         clusterDef.resources().auth.containers["auth-con"].image_tag =
           "node-auth";
-        const localConfig = yield generator._createLocalConfiguration(
-          clusterDef.configuration(),
-          "auth",
-          clusterDef.resources().auth
-        );
-        expect(localConfig).to.exist;
-        expect(localConfig.svc).to.exist;
-        expect(localConfig).to.not.equal(clusterDef.configuration());
-        expect(localConfig.name).to.equal("auth");
-        expect(localConfig["auth-con"].image).to.equal(developImage);
-        expect(localConfig["auth-con"].env).to.include({
-          name: "test",
-          value: "testvalue"
-        });
-        expect(localConfig["auth-con"].env).to.include({
-          name: "ENV_ONE",
-          value: "value-one"
-        });
-        expect(localConfig["auth-con"].env).to.include({
-          name: "ENV_THREE",
-          value: "value-three"
-        });
-        done();
-      })().catch(err => {
-        done(err);
+
+        return generator
+          ._createLocalConfiguration(
+            clusterDef.configuration(),
+            "auth",
+            clusterDef.resources().auth
+          )
+          .should.be.fulfilled.then(localConfig => {
+            expect(localConfig).to.exist;
+            expect(localConfig.svc).to.exist;
+            expect(localConfig).to.not.equal(clusterDef.configuration());
+            expect(localConfig.name).to.equal("auth");
+            expect(localConfig["auth-con"].image).to.equal(developImage);
+            expect(localConfig["auth-con"].env).to.include({
+              name: "test",
+              value: "testvalue"
+            });
+            expect(localConfig["auth-con"].env).to.include({
+              name: "ENV_ONE",
+              value: "value-one"
+            });
+            expect(localConfig["auth-con"].env).to.include({
+              name: "ENV_THREE",
+              value: "value-three"
+            });
+          });
       });
     });
 
-    it("should create copy of config, without plugin", done => {
-      const eventHandler = new EventHandler();
-      return Promise.coroutine(function*() {
-        const clusterDefs = yield yamlHandler.loadClusterDefinitions(
-          "./test/fixture/clusters"
-        );
+    it("should create copy of config, without plugin", () => {
+      return YamlHandler.loadClusterDefinitions(
+        "./test/fixture/clusters"
+      ).should.be.fulfilled.then(clusterDefs => {
         const clusterDef = clusterDefs[3];
         const generator = new Generator(
           clusterDef,
@@ -165,46 +194,49 @@ describe("Generator", () => {
           true,
           undefined,
           undefined,
-          eventHandler
+          new EventHandler()
         );
         expect(clusterDef).to.exist;
+
         if (!fse.existsSync(path.join(os.tmpdir(), clusterDef.name()))) {
           fse.mkdirsSync(path.join(os.tmpdir(), clusterDef.name()));
         }
         // we add the image tag here, since we dont preload the base cluster def in this test
         clusterDef.resources().auth.containers["auth-con"].image_tag =
           "node-auth";
-        const localConfig = yield generator._createLocalConfiguration(
-          clusterDef.configuration(),
-          "auth",
-          clusterDef.resources().auth
-        );
-        expect(localConfig).to.exist;
-        expect(localConfig.svc).to.exist;
-        expect(localConfig).to.not.equal(clusterDef.configuration());
-        expect(localConfig["auth-con"].name).to.equal("auth");
-        expect(localConfig["auth-con"].image).to.equal(developImage);
-        expect(localConfig["auth-con"].env).to.include({
-          name: "test",
-          value: "testvalue"
-        });
-        expect(localConfig["auth-con"].env).to.not.include({
-          name: "ENV_ONE",
-          value: "value-one"
-        });
-        done();
-      })().catch(err => {
-        done(err);
+
+        return generator
+          ._createLocalConfiguration(
+            clusterDef.configuration(),
+            "auth",
+            clusterDef.resources().auth
+          )
+          .should.be.fulfilled.then(localConfig => {
+            expect(localConfig).to.exist;
+            expect(localConfig.svc).to.exist;
+            expect(localConfig).to.not.equal(clusterDef.configuration());
+            expect(localConfig["auth-con"].name).to.equal("auth");
+            expect(localConfig["auth-con"].image).to.equal(developImage);
+            expect(localConfig["auth-con"].env).to.include({
+              name: "test",
+              value: "testvalue"
+            });
+            expect(localConfig["auth-con"].env).to.not.include({
+              name: "ENV_ONE",
+              value: "value-one"
+            });
+          });
       });
     });
 
-    it("should succeed when commitId is specified", done => {
-      const eventHandler = new EventHandler();
-      return Promise.coroutine(function*() {
-        const clusterDefs = yield yamlHandler.loadClusterDefinitions(
-          "./test/fixture/clusters"
-        );
+    it("should set the image as commitId when is passed in", () => {
+      return YamlHandler.loadClusterDefinitions(
+        "./test/fixture/clusters"
+      ).should.be.fulfilled.then(clusterDefs => {
+        const sha = "3154cf1fff0c547c9628c266f6c013b53228fdc8";
         const clusterDef = clusterDefs[3];
+        let auth = clusterDef.cluster.resources["auth"];
+        auth.containers["auth-con"].image_tag = "invision/auth";
         const generator = new Generator(
           clusterDef,
           imageResources,
@@ -213,24 +245,26 @@ describe("Generator", () => {
           true,
           undefined,
           undefined,
-          eventHandler
+          new EventHandler(),
+          undefined,
+          undefined,
+          sha
         );
         expect(clusterDef).to.exist;
-        if (!fse.existsSync(path.join(os.tmpdir(), clusterDef.name()))) {
-          fse.mkdirsSync(path.join(os.tmpdir(), clusterDef.name()));
-        }
-        // we add the image tag here, since we dont preload the base cluster def in this test
-        clusterDef.resources().auth.containers["auth-con"].image_tag =
-          "node-auth";
-        const localConfig = yield generator._createLocalConfiguration(
-          clusterDef.configuration(),
-          "auth",
-          clusterDef.resources().auth
-        );
-        expect(localConfig).to.exist;
-        done();
-      })().catch(err => {
-        done(err);
+
+        return generator
+          ._createLocalConfiguration(
+            clusterDef.configuration(),
+            "auth",
+            clusterDef.resources().auth
+          )
+          .should.be.fulfilled.then(localConfig => {
+            expect(localConfig).to.exist;
+            expect(localConfig.svc).to.exist;
+            expect(localConfig["auth-con"].image).to.equal(
+              `quay.io/invision/auth:release-${sha}`
+            );
+          });
       });
     });
   });
@@ -245,45 +279,38 @@ describe("Generator", () => {
       }
     };
 
-    it("should ignore blank commitId", done => {
-      return Promise.coroutine(function*() {
-        Generator._verifyImagesForCommitId(resourceConfig);
-        Generator._verifyImagesForCommitId(resourceConfig, null);
-        done();
-      })().catch(err => {
-        done(err);
+    it("should ignore blank commitId", () => {
+      return Promise.resolve(
+        Generator._verifyImagesForCommitId(resourceConfig)
+      ).should.be.fulfilled.then(() => {
+        return Promise.resolve(
+          Generator._verifyImagesForCommitId(resourceConfig, null)
+        ).should.be.fulfilled;
       });
     });
 
-    it("should ignore a ResourceConfig with no containers", done => {
-      return Promise.coroutine(function*() {
-        Generator._verifyImagesForCommitId({}, "abc1");
-        done();
-      })().catch(err => {
-        done(err);
-      });
+    it("should ignore a ResourceConfig with no containers", () => {
+      return Promise.resolve(Generator._verifyImagesForCommitId({}, "abc1"))
+        .should.be.fulfilled;
     });
 
-    it("should fail for the wrong commitId", done => {
-      return Promise.coroutine(function*() {
+    it("should fail for the wrong commitId", () => {
+      try {
         Generator._verifyImagesForCommitId(resourceConfig, "wrong");
-        done(
-          new Error(
-            "Expected _verifyImagesForCommitId to throw an error because it was for the wrong commitId."
-          )
+      } catch (err) {
+        expect(err.message).to.be.equal(
+          `This kit manifest generation was for commitId 'wrong', but none of the SHAs from images (abc1,abc2) match that.`
         );
-      })().catch(err => {
-        done();
-      });
+      }
     });
 
-    it("should succeed for the right commitId(s)", done => {
-      return Promise.coroutine(function*() {
-        Generator._verifyImagesForCommitId(resourceConfig, "abc1");
-        Generator._verifyImagesForCommitId(resourceConfig, "abc2");
-        done();
-      })().catch(err => {
-        done(err);
+    it("should succeed for the right commitId(s)", () => {
+      return Promise.resolve(
+        Generator._verifyImagesForCommitId(resourceConfig, "abc1")
+      ).should.be.fulfilled.then(() => {
+        return Promise.resolve(
+          Generator._verifyImagesForCommitId(resourceConfig, "abc2")
+        ).should.be.fulfilled;
       });
     });
   });
