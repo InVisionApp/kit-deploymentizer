fae8a1dfe69b16a842cd10f371a687d412511f7b
Manuel Alonso <manuelalonso@invisionapp.com>
2018-08-15 18:36:18 +0200

    removing fallbacks

--- test/unit/plugin/env-api-client-v3.spec.js
+++ test/unit/plugin/env-api-client-v3.spec.js
@@ -83,22 +83,6 @@ describe("ENV API Client Configuration plugin", () => {
       });
     });
 
-    const resV1Env = new Promise((resolve, reject) => {
-      resolve({
-        env: {
-          GET_HOSTS_FROM: "dns",
-          MAX_RETRIES: "0",
-          MEMBER_HOSTS:
-            "mongoreplica-01-svc:27017,mongoreplica-02-svc:27017,mongoreplica-03-svc:27017",
-          REPLICA_SET_NAME: "rs0",
-          WAIT_TIME: "60000"
-        },
-        k8s: {
-          branch: "develop"
-        }
-      });
-    });
-
     const testrosieService = {
       name: "testrosie",
       annotations: {
@@ -211,10 +195,9 @@ describe("ENV API Client Configuration plugin", () => {
         });
     });
 
-    it("should call request to v3 and fallback to v1", () => {
+    it("should fail when error request to v3", () => {
       var rp = sinon.stub();
       rp.onFirstCall().returns(resV3Invalid);
-      rp.onSecondCall().returns(resV1Env);
       const cluster = {
         kind: "ClusterNamespace",
         metadata: {
@@ -232,56 +215,19 @@ describe("ENV API Client Configuration plugin", () => {
       const clusterDef = new ClusterDefinition(cluster, config);
 
       const options = {
-        apiUrl: "https://envapi.tools.shared-multi.k8s.invision.works/api",
-        supportFallback: true
+        apiUrl: "https://envapi.tools.shared-multi.k8s.invision.works/api"
       };
       const apiConfig = new ApiConfig(options);
       apiConfig.request = rp;
 
-      apiConfig
+      return apiConfig
         .fetch(testService, clusterDef)
-        .should.be.fulfilled.then(envs => {
-          expect(rp.callCount).to.equal(2);
-          expect(envs.env.length).to.equal(5);
-          expect(envs.env[0].name).to.equal("GET_HOSTS_FROM");
-          expect(envs.env[0].value).to.equal("dns");
-          expect(envs.env[1].name).to.equal("MAX_RETRIES");
-          expect(envs.env[1].value).to.equal("0");
+        .should.be.rejected.then(err => {
+          expect(rp.callCount).to.equal(1);
+          expect(err.message).to.equal(
+            "Unable to fetch 'in-config.yaml' from 'node-test-rosie' repo"
+          );
         });
-    });
-
-    it("should call request to v3 and no fallback", () => {
-      let rp = sinon.stub();
-      rp.onFirstCall().returns(resV3Invalid);
-      rp.onSecondCall().returns(resV1Env);
-      const cluster = {
-        kind: "ClusterNamespace",
-        metadata: {
-          name: "staging-cluster",
-          type: "staging",
-          environment: "staging",
-          domain: "somewbesite.com",
-          restricted: true
-        }
-      };
-      const config = {
-        kind: "ResourceConfig",
-        env: [{ name: "a", value: 1 }, { name: "b", value: 2 }]
-      };
-      const clusterDef = new ClusterDefinition(cluster, config);
-
-      const options = {
-        apiUrl: "https://envapi.tools.shared-multi.k8s.invision.works/api",
-        supportFallback: false
-      };
-      const apiConfig = new ApiConfig(options);
-      apiConfig.request = rp;
-
-      apiConfig
-        .fetch(testService, clusterDef)
-        .should.be.rejectedWith(
-          "Fallback not supported and/or wrong error code 404: Unable to fetch 'in-config.yaml' from 'node-test-rosie' repo"
-        );
     });
   });
 
